d9df3a8fef1675beb7684a3131f175165465f399
jeevan-budha <jeevan.7budha@gmail.com>
2025-06-23 18:27:48 +0545

    dark light feature added

--- app.js
+++ app.js
@@ -1,35 +1,34 @@
 
 const countryContainer = document.querySelector(".countries-container");
-
 const filterByRegion = document.querySelector(".Filter-by-region");
-
 const searchCountry = document.querySelector(".search-container input");
+const themeChanger = document.querySelector(".theme-changer");
 
 let allContriesData;
 
 fetch("./data.json").then((res) => res.json())
-  .then((data)=>{
+  .then((data) => {
     renderCardCountry(data)
-    allContriesData=data;
+    allContriesData = data;
   })
 
 
-  filterByRegion.addEventListener("change",(e)=>{
-    console.log(e.target.value);
-    fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`).then((res) => res.json())
-  .then(renderCardCountry);
-  })
+filterByRegion.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`).then((res) => res.json())
+    .then(renderCardCountry);
+})
 
 
-function renderCardCountry(data){
-  countryContainer.innerHTML=``;
-   data.forEach((country) => {
-      const {name, flag,population,region,capital}= country;
+function renderCardCountry(data) {
+  countryContainer.innerHTML = ``;
+  data.forEach((country) => {
+    const { name, flag, population, region, capital } = country;
 
-      const countryCard = document.createElement('a');
-      countryCard.classList.add("country-card");
-      countryCard.href=`country.html?name=${name}`
-      const cardHTML = `
+    const countryCard = document.createElement('a');
+    countryCard.classList.add("country-card");
+    countryCard.href = `country.html?name=${name}`
+    const cardHTML = `
        <img src="${flag}" alt="flag">
         <div class="card-text">
           <h3>${name}</h3>
@@ -38,12 +37,22 @@ function renderCardCountry(data){
           <P><b>Capital: </b><span>${capital}</span></P>
         </div>
 `
-      countryCard.innerHTML = cardHTML;
-      countryContainer.append(countryCard);
-    });
+    countryCard.innerHTML = cardHTML;
+    countryContainer.append(countryCard);
+  });
 }
 
-searchCountry.addEventListener("input",(e)=>{
-  const filterCountires = allContriesData.filter((country)=>country.name.toLowerCase().includes(e.target.value.toLowerCase()));
+searchCountry.addEventListener("input", (e) => {
+  const filterCountires = allContriesData.filter((country) => country.name.toLowerCase().includes(e.target.value.toLowerCase()));
   renderCardCountry(filterCountires);
+})
+
+themeChanger.addEventListener("click", () => {
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    themeChanger.innerHTML = `<i class="fa-solid fa-sun"></i>&nbsp;&nbsp;Light Mode`;
+  } else {
+    themeChanger.innerHTML = `<i class="fa-solid fa-moon"></i>&nbsp;&nbsp;Dark Mode`;
+  }
 })
