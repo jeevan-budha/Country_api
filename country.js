7c841bb64793089fd5dfc2359925768ee8c40558
jeevan-budha <jeevan.7budha@gmail.com>
2025-06-23 10:55:29 +0545

    add file for show dynamic counrty details

---
+++ country.js
@@ -0,0 +1,10 @@
+
+
+let countryName= new URLSearchParams(location.search).get('name');
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res)=>res.json())
+.then((data)=>{
+  console.log(data[0]);
+})
+
+console.log(new URLSearchParams(location.search).get('name'));
